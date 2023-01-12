fbe95def54df66b67927c1ce164e30f7cbd07bf0
ujwal-akare <120369418+ujwal-akare@users.noreply.github.com>
2023-01-12 18:32:25 +0530

    Update PhotozClientAuthzTestApp.java

--- testsuite/integration-arquillian/tests/base/src/main/java/org/keycloak/testsuite/adapter/page/PhotozClientAuthzTestApp.java
+++ testsuite/integration-arquillian/tests/base/src/main/java/org/keycloak/testsuite/adapter/page/PhotozClientAuthzTestApp.java
@@ -33,7 +33,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
-import java.util.io.*
+import java.util.io.*;
 import java.net.URL;
 
 import static org.junit.Assert.assertEquals;
